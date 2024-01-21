f4bd84ffc34d579131128bc506fa8911baa72678
Ziad Hammouri <ziadhammouri@gmail.com>
2024-01-21 21:52:40 +0100

    final change main

--- first_pro.py
+++ first_pro.py
@@ -7,12 +7,8 @@ print('hi world')
 print ('first change')
 
 print ('2nd change')
-<<<<<<< HEAD
 
 #
 print ('change in the main after create branch and add to it')
-=======
-##########
-print ('i add branch ')
-print ('change in branch first after creat seoncd branch')
->>>>>>> first_branch
+
+print ('now there is conflict in main')
