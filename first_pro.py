0fba57d7d4a33fa7b012cb9caeeb4a79464f5abf
Ziad Hammouri <ziadhammouri@gmail.com>
2024-01-21 22:28:51 +0100

    delete sum comments

--- first_pro.py
+++ first_pro.py
@@ -4,19 +4,6 @@ import os
 
 print('hi world')
 
-print ('first change')
-
-print ('2nd change')
-##########
-print ('i add branch ')
-print ('change in branch first after creat seoncd branch')
-print ('check conflict final')
-
-#
-print ('change in the main after create branch and add to it')
-
-print ('now there is conflict in main')
-#
 def sum(x,y):
     return x+y
 
